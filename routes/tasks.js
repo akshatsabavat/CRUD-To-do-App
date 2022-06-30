689b2db0acffd84e83358a06f0f1dc9d50cf8582
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-01 01:16:27 +0530

    task api GET request successfull

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const Tasks = require("../models/taskModels");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send({ message: "Hey there" });
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Tasks.find();
+    if (tasks == "") {
+      res.send({ message: "no tasks as of now" });
+    } else {
+      res.send(tasks);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong with the server" });
+  }
 });
 
 module.exports = router;
