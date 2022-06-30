414720885a5c6223f1be27b4ee866722883a8336
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-01 01:04:29 +0530

    Server connection successfull

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const tasksRoute = require("./routes/tasks");
 const app = express();
 const port = 3000;
 
@@ -12,6 +13,7 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connection to database successfull"));
 
 app.use(bodyParser.json());
+app.use("/tasks", tasksRoute);
 app.listen(port, () => {
   console.log(`Server now live on: ${port}`);
 });
