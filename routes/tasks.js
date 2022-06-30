f20f1df274c91d226f91dcbf18d59f5310c7789a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-01 02:35:18 +0530

    API completed

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,6 @@
 const express = require("express");
-var taskController = require("../controllers/tasksController");
 const router = express.Router();
+var taskController = require("../controllers/tasksController");
 
 //all routes
 router.post("/", taskController.addTask);
