2b022c7baa280a219ac9495e023e1b7f14cb3165
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-02 21:17:06 +0530

    API URL testing successfull

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Axios from "axios";
+
+const getAPI = () => {
+  Axios.get("http://localhost:3000/tasks").then((res) => console.log(res));
+};
 
 function App() {
   return (
     <div className="App">
       <h1>Task app</h1>
+      <button className="App-Btn" onClick={getAPI}>
+        Load Data
+      </button>
     </div>
   );
 }
