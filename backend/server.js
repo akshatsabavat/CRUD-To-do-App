d0c528638fdba71e38a830eaa6064cb8cd40289d
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-02 21:16:36 +0530

    added cors for frontend connection

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const tasksRoute = require("./routes/tasks");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -13,6 +14,7 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("connection to database successfull"));
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/tasks", tasksRoute);
 app.listen(port, () => {
   console.log(`Server now live on: ${port}`);
