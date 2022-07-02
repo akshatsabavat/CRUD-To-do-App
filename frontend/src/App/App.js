0afa7d97878e5a1bed1c8002c400725973ace28d
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-02 22:03:02 +0530

    hook testing successfull

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
-import Axios from "axios";
+import useAPI from "../Hooks/useAPI";
 
-const getAPI = () => {
-  Axios.get("http://localhost:3000/tasks").then((res) => console.log(res.data));
+const getAPI = (apiData, isloading) => {
+  if (isloading) {
+    console.log(isloading);
+  } else {
+    console.log(apiData);
+  }
 };
 
 const App = () => {
+  const { isloading, error, apiData } = useAPI("http://localhost:3000/tasks");
   return (
     <div className="App">
       <h1>Task app</h1>
-      <button className="App-Btn" onClick={getAPI}>
+      <button className="App-Btn" onClick={() => getAPI(apiData, isloading)}>
         Load Data
       </button>
     </div>
