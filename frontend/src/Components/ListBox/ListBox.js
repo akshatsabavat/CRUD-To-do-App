18e8472604d22ccddd574ee2c22493b037ffac5a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-07-04 02:48:56 +0530

    add task code added

--- frontend/src/Components/ListBox/ListBox.js
+++ frontend/src/Components/ListBox/ListBox.js
@@ -1,29 +1,15 @@
-import useAPI from "../../Hooks/useAPI";
 import "../ListBox/ListBox.css";
 
 const ListBox = (props) => {
-  const { setStatus } = useAPI("http://localhost:3000/tasks");
   return (
     <div className="box-Container">
       <p className={props.taskStatus ? "listname-done" : "listname"}>
         {props.listName}
       </p>
       <div className="cta-Container">
-        <button
-          onClick={() =>
-            setStatus("http://localhost:3000/tasks/completed", props.taskID)
-          }
-        >
-          Mark Done
-        </button>
-        <button
-          onClick={() =>
-            setStatus("http://localhost:3000/tasks/undone", props.taskID)
-          }
-        >
-          Mark UnDone
-        </button>
-        <button onClick={() => props.removeFunction}>Remove Task</button>
+        <button onClick={props.completeFunction}>Mark Done</button>
+        <button onClick={props.undoFunction}>Mark UnDone</button>
+        <button>Remove Task</button>
       </div>
     </div>
   );
--- frontend/src/Containers/listContainer/listsContainer.js
+++ frontend/src/Containers/listContainer/listsContainer.js
@@ -1,9 +1,80 @@
 import ListBox from "../../Components/ListBox/ListBox";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 const ListContainer = () => {
+  const [apiData, setApiData] = useState([]);
+  const [newTodo, setNewTodo] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get("http://localhost:3000/tasks");
+      const data = await response.data;
+      setApiData(data);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    }
+  };
+
+  const completeTask = async (taskID) => {
+    await axios.patch("http://localhost:3000/tasks/completed", {
+      id: taskID,
+    });
+
+    getData();
+  };
+
+  const UndoTask = async (taskID) => {
+    await axios.patch("http://localhost:3000/tasks/undone", {
+      id: taskID,
+    });
+
+    getData();
+  };
+
+  // const deleteTask = async (taskID) => {
+  //   const response = await axios.delete("http://localhost:3000/tasks", {
+  //     id: taskID,
+  //   });
+  //   const data = await response.data;
+
+  //   console.log(data);
+  // };
+
+  const addTask = async (taskName) => {
+    setIsLoading(true);
+    try {
+      await axios.post("http://localhost:3000/tasks", {
+        taskName: taskName,
+        taskState: false,
+      });
+      getData();
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err);
+    }
+  };
+
   return (
     <div className="Tasks container">
+      <input
+        type="text"
+        className="inputFeild"
+        onChange={(e) => setNewTodo(e.target.value)}
+        value={newTodo}
+      />
+      <button className="addBtn" onClick={() => addTask(newTodo)}>
+        add task
+      </button>
       {isloading ? (
         <p>Loading....</p>
       ) : (
@@ -11,6 +82,8 @@ const ListContainer = () => {
           {apiData.map((task) => {
             return (
               <ListBox
+                completeFunction={() => completeTask(task._id)}
+                undoFunction={() => UndoTask(task._id)}
                 taskStatus={task.taskState}
                 taskID={task._id}
                 key={task._id}
